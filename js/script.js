386544ec6b8ace09ef9d0a308c53b4bfd032b803
POLLY1515 <amarantepolly2030@gmail.com>
2023-08-19 23:19:57 -0300

    Iniciando o javascript

---
+++ js/script.js
@@ -0,0 +1,45 @@
+//Seleção de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+
+
+
+
+
+
+//Funções
+
+
+
+
+
+
+
+
+
+//Eventos
+todoForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    const inputValue = todoInput.value//pegando o valor do input
+    if(inputValue){
+        console.log(inputValue)
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
